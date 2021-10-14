8c07fdde13d1aaaa5e0e8e00376b14223de4607f
jexeq <jerexand@gmail.com>
2021-10-13 21:50:01 -0300

    added responsiveness at ReportsBySection and some refactor

--- mas-noticias/client/src/views/ReportsBySection.jsx
+++ mas-noticias/client/src/views/ReportsBySection.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getReportsBySection, clearReports } from '../redux/actions/report/reportActions';
+import { getActivePublicities, clearPublicity } from "../redux/actions/publicity/publicityActions";
 import MainReportCard from '../components/report/mainReportCard/MainReportCard';
 import './reportBySection.css';
 
@@ -9,26 +10,33 @@ export default function ReportsBySection (props) {
     const {sectionId} = props.match.params;
     const dispatch = useDispatch();
     const storeReports = useSelector( state => state.reportReducer.reports);
+    const storePublicities = useSelector( state => state.publicityReducer.publicities);
     const [loading, setLoading] = useState(true);
 
     useEffect(()=>{
-        dispatch(clearReports())
+        
+        dispatch(getActivePublicities())
         dispatch(getReportsBySection(sectionId))
-        console.log("se montó ReportsBySection")
-        return ()=> dispatch(clearReports())
+        
+        return ()=> {
+            dispatch(clearPublicity());
+            dispatch(clearReports());
+        }
     },[])
 
     useEffect(()=>{
-        console.log("storeReports", storeReports)
+        
         if(storeReports) {
+            if(storePublicities) {
 
-            setLoading(false);
+                setLoading(false);
+            }
         }
     },[storeReports])
 
     return !loading&&(
-        <div className='container'>
-            reportes
+        <div className='center-col'>
+            
             {storeReports&&storeReports.map( r=> {
                 return (
                 <div  className='container'>
